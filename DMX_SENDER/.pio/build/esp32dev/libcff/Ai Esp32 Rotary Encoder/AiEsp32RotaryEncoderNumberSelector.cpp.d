8e0d93d4c1513c47a8188e71406beafb627fc303
Benz <benz.cramer001@stud.fh-dortmund.de>
2023-08-08 13:30:13 +0200

    Test der neuen Gitignore

--- DMX_SENDER/.pio/build/esp32dev/libcff/Ai Esp32 Rotary Encoder/AiEsp32RotaryEncoderNumberSelector.cpp.d
+++
@@ -1,2 +0,0 @@
-.pio/build/esp32dev/libcff/Ai\ Esp32\ Rotary\ Encoder/AiEsp32RotaryEncoderNumberSelector.cpp.o: \
- .pio/libdeps/esp32dev/Ai\ Esp32\ Rotary\ Encoder/src/AiEsp32RotaryEncoderNumberSelector.cpp
